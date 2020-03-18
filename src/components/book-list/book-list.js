34f100665e04b53414b7019e099771bda17eb176
Ilya Volkov <volkov-ilyha@ya.ru>
2020-03-18 22:48:40 +0300

    Получает данные из контекста и передаёт их в Redux Store

--- src/components/book-list/book-list.js
+++ src/components/book-list/book-list.js
@@ -2,10 +2,20 @@ import React, { Component } from 'react';
 import { connect } from 'react-redux';
 
 import BookListItem from './../book-list-item';
+import { withBookstoreService } from './../hoc';
+import { booksLoaded } from './../../actions';
+import { compose } from './../../utils';
 
 import './book-list.css';
 
 class BookList extends Component {
+
+  componentDidMount() {
+    const { bookstoreService } = this.props;
+    const data = bookstoreService.getBooks();
+
+    this.props.booksLoaded(data);
+  }
 
   render() {
     const { books } = this.props;
@@ -27,4 +37,11 @@ const mapStateToProps = ({ books }) => {
   return { books };
 };
 
-export default connect(mapStateToProps)(BookList);
+const mapDispatchToProps = {
+  booksLoaded
+};
+
+export default compose (
+  withBookstoreService(),
+  connect(mapStateToProps, mapDispatchToProps)
+)(BookList);
